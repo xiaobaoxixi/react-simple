14972fa23c41df96802cdf6e5b687a2a0c485291
xiaobaoxixi <yanli.xiaobaoxixi@gmail.com>
2018-11-10 20:24:57 +0100

    added: sort item by importance

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,16 +4,20 @@ import "./TodoList.css";
 
 export class ToDoList extends Component {
   render() {
-    const allEntriesToEle = this.props.entries.map(entry => {
-      return (
-        <ToDoItem
-          entry={entry}
-          markDone={this.props.markDone}
-          key={entry.uid}
-          state={entry.done}
-        />
-      );
-    });
+    const allEntriesToEle = this.props.entries
+      .sort(function(a, b) {
+        return b.importance - a.importance;
+      })
+      .map(entry => {
+        return (
+          <ToDoItem
+            entry={entry}
+            markDone={this.props.markDone}
+            key={entry.uid}
+            state={entry.done}
+          />
+        );
+      });
     return <ul>{allEntriesToEle}</ul>;
   }
 }
